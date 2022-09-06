9cf531f4223c4a9ed8b0284d564acd161ad22fe1
Nizam Khan <nkhan@vcasoftware.com>
2022-09-06 18:11:55 +0530

    Removed other projects

--- Patterns.NullDesignPattern/DbContext/Interfaces/IContext.cs
+++
@@ -1,9 +0,0 @@
-﻿using Patterns.NullDesignPattern.Contracts.Models;
-
-namespace Patterns.NullDesignPattern.DbContext.Interfaces
-{
-    public interface IContext
-    {
-        List<Product> GetProducts();
-    }
-}
--- Patterns.NullDesignPattern/Repositories/Interfaces/IProductRepository.cs
+++
@@ -1,8 +0,0 @@
-﻿using Patterns.NullDesignPattern.Contracts.Interfaces;
-namespace Patterns.NullDesignPattern.Repositories.Interfaces
-{
-    public interface IProductRepository
-    {
-        IProduct GetProductById(int ProductId);
-    }
-}
