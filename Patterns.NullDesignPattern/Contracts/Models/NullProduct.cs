69d699d84ef050a510f5adeb6f30f183bb0ba20d
Nizam Khan <nkhan@vcasoftware.com>
2022-09-06 16:58:37 +0530

    Implemented Null design pattern in seperate project

---
+++ Patterns.NullDesignPattern/Contracts/Models/NullProduct.cs
@@ -0,0 +1,18 @@
+﻿using Patterns.NullDesignPattern.Contracts.Interfaces;
+namespace Patterns.NullDesignPattern.Contracts.Models
+{
+    /// <summary>
+    /// Class which implements Null design pattern
+    /// </summary>
+    public class NullProduct : IProduct
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; } = string.Empty;
+        public double GetPrice() => default;
+        /// <summary>
+        /// Always true for Null object
+        /// </summary>
+        public bool IsNull => true;
+    }
+
+}
